bd50a9216954f363e6801753d4e9a3ac8f2e92eb
vaquero <froilan@virtual-artifact.com>
2023-07-26 21:31:15 +0100

    add minimal styles to prompt area

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -1,12 +1,12 @@
 export default function Playground(){
   return (
     <main className="flex-min-h-screen flex-col items-center justify-between p-24">
-      <section>
+      <section className="min-h-screen justify-between bg-slate-200 w-96 mx-auto p-8">
         <div>
           <h1>Playground</h1>
           <h2>Happy Prompting!!</h2>
         </div>
-        <div>Enter a prompt to get started</div>
+        <div className="min-h-[300px]">Enter a prompt to get started</div>
         <form id="form">
           <input type="text" id="prompt" />
           <button type="submit">Submit</button>
