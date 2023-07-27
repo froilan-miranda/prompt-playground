32557d1d49d58ab9286484f2fb57235ad40dc727
vaquero <froilan@virtual-artifact.com>
2023-07-28 00:18:41 +0100

    fix parameter missing type info

--- app/playground/page.tsx
+++ app/playground/page.tsx
@@ -20,7 +20,7 @@ export default function Playground(){
   })
    */
 
-  const fetchPropmtReply = async (e: React.FormEvent<HTMLInputElement>) => {
+  const fetchPropmtReply = async (e: React.FormEvent<HTMLFormElement>) => {
     e.preventDefault()
     console.log('ready to fetch')
     const url = 'https://gleaming-entremet-fd05b3.netlify.app/.netlify/functions/fetch-ai-response'
