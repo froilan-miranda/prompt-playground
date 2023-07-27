0e4964d30ef6a03bddd00172d55bef94a003e586
vaquero <froilan@virtual-artifact.com>
2023-07-27 14:02:36 +0100

    redo api endpoint to work better with netlify service

--- netlify/functions/fetch-ai-response/fetch-ai-response.js
+++ netlify/functions/fetch-ai-response/fetch-ai-response.js
@@ -1,20 +1,70 @@
 import { initializeApp } from 'firebase/app'
 import { getDatabase, ref, push, get, remove } from 'firebase/database'
 import { Configuration, OpenAIApi } from 'openai'
+  
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+})
 
+const appSettings = {
+    databaseURL: 'https://prompt-playground-97436-default-rtdb.firebaseio.com/'
+}
+const openai = new OpenAIApi(configuration)
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const conversationInDb = ref(database)
+
+const instructionObj = {
+    role: 'system',
+    content: 'You are a helpful assistant.'
+}
 const handler = async (event) => {
   try {
-    const subject = event.queryStringParameters.name || 'World'
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ message: `Hello ${subject}` }),
-      // // more keys you can return:
-      // headers: { "headerName": "headerValue", ... },
-      // isBase64Encoded: true,
-    }
+    pushConversation(event.body)
+    return handleConversationReply()
   } catch (error) {
     return { statusCode: 500, body: error.toString() }
   }
 }
 
-module.exports = { handler }
+function handleConversationReply(){
+  return get(conversationInDb).then(async (snapshot) => {
+        if (snapshot.exists()) {
+          const conversationArr = Object.values(snapshot.val())
+          conversationArr.unshift(instructionObj)
+          const response =  await fetchOpenAI(conversationArr)
+          push(conversationInDb, response.data.choices[0].message)
+          return {
+            statusCode: 200,
+            body: JSON.stringify({ reply: response.data }),
+          }
+        } else {
+          return {
+            statusCode: 404,
+            body: JSON.stringify({ error: 'Conversation does not exist' }),
+          }
+        }
+    })
+
+}
+
+function fetchOpenAI(array){
+  return openai.createChatCompletion({
+            model: 'gpt-3.5-turbo',
+            messages: array,
+            presence_penalty: 0,
+            frequency_penalty: 0.3,
+            max_tokens: 500,
+            temperature: 0,
+          }) 
+}
+
+function pushConversation(input) {
+  push(conversationInDb, {
+    role: 'user',
+    content: input
+  })
+}
+
+export { handler }
